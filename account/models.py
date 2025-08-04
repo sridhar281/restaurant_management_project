12385e2ed59b48e18e7074e78e4824c63cf5f934
Sridhar. <135013637+sridhar281@users.noreply.github.com>
2025-08-04 22:52:57 +0530

    Update models.py
    Created a simple user profile

--- account/models.py
+++ account/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15)
+    
+    def __str__(self):
+        return self.name
