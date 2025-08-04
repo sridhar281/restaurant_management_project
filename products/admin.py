9c84fe178505545eadbdb94ef476e18fa7293360
Sridhar. <135013637+sridhar281@users.noreply.github.com>
2025-08-04 23:13:47 +0530

    17467

--- products/admin.py
+++ products/admin.py
@@ -5,7 +5,14 @@ from .models import *
 # Custom Admins
 class ItemAdmin(admin.ModelAdmin):
     list_display = ['item_name','item_price','created_at']
-
+    search_fields = ('name')
 
 # Register your models here.
+@admin.register(Order)
+class OrderAdmin(admin,ModelAdmin):
+    list_display=('id','customer_name','created_at')
+    search_fields=('customer_name')
+    filter_horizontal=('items,')
+
+    
 admin.site.register(Item,ItemAdmin)
