dac2efcf5ca2abffc51eae56d339eca42951c530
Sridhar. <135013637+sridhar281@users.noreply.github.com>
2025-08-04 23:07:39 +0530

    17466

--- orders/models.py
+++ orders/models.py
@@ -11,7 +11,7 @@ class Order(models.Model):
     ]
 
     customer=models.ForeignKey(User,on_delete=models.CASCADE,related_name='orders')
-    tot_amount=models.DecomalField(max_digits=8,decimal_places=2)
+    tot_amount=models.DecimalField(max_digits=8,decimal_places=2)
     status=models.CharField(max_length=10,choices=STATUS_CHOICES,default='PENDING')
     created_at=models.DateTimeField(auto_now_add=True)
 
@@ -22,10 +22,10 @@ class Order(models.Model):
 class OrderItem(models.model):
     order=models.ForeignKey(Order,on_delete=models.CASCADE,related_name='items')
     item=models.ForeignKey(Item,on_delete=models.CASCADE)
-    quantity=models.PositiveIntegerField(default=1)
+    quantity_ordered=models.PositiveIntegerField(default=0)
 
     def __str__(self):
-        return f"{self.quantity}*{self.item.name}"
+        return f"{self.quantity_ordered}*{self.item.name}"
 
         
 
