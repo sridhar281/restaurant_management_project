109e2ed06786d75a923f1bbe7181dab2c239220b
Sridhar. <135013637+sridhar281@users.noreply.github.com>
2025-08-04 23:18:34 +0530

    Update urls.py

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import *
+from .views import homepage, HardcodedMenuView
 
 urlpatterns = [
-    path('items/', ItemView.as_view(), name='item-list'),
+      path('items/', ItemView.as_view(), name='item-list'),
+    path('', homepage, name='homepage'),
+    path('menu-static/', HardcodedMenuView.as_view(), name='hardcoded-menu'),
 ]
+
